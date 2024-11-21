ad4830b21ab85b6bd28c7d8244aed5938203f2b5
Hieu Nguyen <hieulechanhkk@gmail.com>
2024-11-21 22:06:39 +0700

    add rule for clean architecture

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
     tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: ['@typescript-eslint/eslint-plugin', 'boundaries'],
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
@@ -21,5 +21,42 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+
+    // Boundaries Clean Architecture
+    'boundaries/element-types': [
+      'error',
+      {
+        default: 'disallow',
+        rules: [
+          // Presentation -> Application
+          { from: 'presentation', allow: ['application', 'shared'] },
+          
+          // Application -> (Domain & Shared)
+          { from: 'application', allow: ['domain', 'shared'] },
+          
+          // Domain -> Shared
+          { from: 'domain', allow: ['shared'] },
+
+          // Infrastructure -> Application & Shared
+          { from: 'infrastructure', allow: ['application', 'shared'] },
+
+          // Shared can use any other layer
+          { from: 'shared', allow: ['presentation', 'application', 'domain', 'infrastructure'] },
+        ],
+      },
+    ],
   },
+
+  // Patterns
+  settings: {
+    boundaries: {
+      types: [
+        { type: 'presentation', pattern: 'src/presentation/*' },
+        { type: 'application', pattern: 'src/application/*' },
+        { type: 'domain', pattern: 'src/domain/*' },
+        { type: 'infrastructure', pattern: 'src/infrastructure/*' },
+        { type: 'shared', pattern: 'src/shared/*' },
+      ]
+    }
+  }
 };
